354708372368e44e7787531cbdb2c2d4a5155612
Tatyana-Petkova <t_vesselinova@abv.bg>
2020-12-13 03:05:38 +0200

    final touch: change colors and translate text and button's labels in all  html pages and navbar.html

--- users/models.py
+++ users/models.py
@@ -19,17 +19,5 @@ class Results(models.Model):
 
     user = models.ForeignKey(RegisteredUser, null=True, on_delete=models.CASCADE)
 
-    # def __str__(self):
-    #     return self.user
-
-
-# class ImportedResults(models.Model):
-#     user = models.ForeignKey(RegisteredUser, null=True, on_delete=models.CASCADE)
-#     results = models.ForeignKey(Results, null=True, on_delete=models.CASCADE)
-#     result_value = models.FloatField(null=True)
-#     date_created = models.DateTimeField(auto_now_add=True, null=True)
-#
-#     def __str__(self):
-#         #return f'{self.user} - {self.results} - {self.result_value} - {self.date_created}'
-#         return f'{self.results} - {self.result_value}'
-
+    def __str__(self):
+        return f"{self.user} - {self.date_created}"
